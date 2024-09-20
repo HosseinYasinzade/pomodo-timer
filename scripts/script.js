cc5216ebecac6eb991c792879b3dfb655209594b
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-09-20 11:03:12 +0330

    set time debug

--- scripts/script.js
+++ scripts/script.js
@@ -16,13 +16,21 @@ btn.addEventListener("click", function (e) {
   if (
     secondeInput.value > 60 ||
     MinuteInput.value < 0 ||
-    secondeInput.value < 0
+    secondeInput.value < 0 ||
+    MinuteInput.value === "" ||
+    secondeInput.value === ""
   ) {
     body.style.backgroundColor = "red";
     timer.style.color = "red";
     MinuteInput.value = "";
     secondeInput.value = "";
+    title.style.color = "red";
+    title.textContent = "Please enter valid time ....";
   } else {
+    body.style.backgroundColor = "#6a1b9a";
+    timer.style.color = "#0288d1";
+    title.textContent = "";
+    title.color = "#0288d1";
     setTimer(MinuteInput.value, secondeInput.value);
   }
 });
