c493d5116c583f148858c6dfd22460fdb84e8f67
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-09-20 15:28:49 +0330

    debug input

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,33 @@
 const body = document.querySelector("body");
-let MinuteInput = document.querySelector(".task_minute");
-let secondeInput = document.querySelector(".task_seconde");
+let MinuteInput = document.querySelector(".task_minute").value;
+let secondeInput = document.querySelector(".task_seconde").value;
 const btn = document.querySelector(".set_btn");
 const timer = document.querySelector(".timer");
 const title = document.querySelector(".task-paragraph");
 
 let intervalId;
+let minute = Number(MinuteInput);
+let seconde = Number(secondeInput);
 
 const setTimer = (min, sec) => {
   timer.textContent = `${min} : ${sec}`;
 };
 
 const startTimer = () => {
-  let min = Number(MinuteInput.value);
-  let sec = Number(secondeInput.value);
-
-  if (sec <= 0 && min <= 0) {
+  if (seconde <= 0 && minute <= 0) {
     clearInterval(intervalId);
     title.textContent = "Time End";
     return;
   }
 
-  if (sec === 0) {
-    min--;
-    sec = 59;
+  if (seconde === 0) {
+    minute--;
+    seconde = 59;
   } else {
-    sec--;
+    seconde--;
   }
 
-  MinuteInput.value = min;
-  secondeInput.value = sec;
-  timer.textContent = `${min} : ${sec}`;
+  timer.textContent = `${minute} : ${seconde}`;
 };
 
 btn.addEventListener("click", function (e) {
@@ -53,7 +50,7 @@ btn.addEventListener("click", function (e) {
     body.style.backgroundColor = "#6a1b9a";
     timer.style.color = "#0288d1";
     title.textContent = "";
-    setTimer(MinuteInput.value, secondeInput.value);
+    setTimer(minute, seconde);
 
     intervalId = setInterval(startTimer, 1000);
   }
